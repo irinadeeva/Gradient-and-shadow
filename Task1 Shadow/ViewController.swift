c3fdd3faafd3b56dd34a639218d035b70e73d57a
irinadeeva <deeva97@me.com>
2024-11-01 13:30:58 +0300

    Initial Commit

---
+++ Task1 Shadow/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task1 Shadow
+//
+//  Created by Irina Deeva on 01/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Task1 ShadowUITests/Task1_ShadowUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Task1_ShadowUITestsLaunchTests.swift
+//  Task1 ShadowUITests
+//
+//  Created by Irina Deeva on 01/11/24.
+//
+
+import XCTest
+
+final class Task1_ShadowUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}

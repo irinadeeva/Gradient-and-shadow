c894c60c41ada30b33c8b2c4006da22227c7ed7a
irinadeeva <deeva97@me.com>
2024-11-01 16:26:19 +0300

    added custom colors, done square view UI

--- Task1 Shadow/ViewController.swift
+++ Task1 Shadow/ViewController.swift
@@ -8,12 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private let squareView = UIView()
+    private let gradient = CAGradientLayer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        squareView.layer.cornerRadius = 16
+
+        squareView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(squareView)
+
+        squareView.heightAnchor.constraint(equalToConstant: 125).isActive = true
+        squareView.widthAnchor.constraint(equalToConstant: 125).isActive = true
+        squareView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        squareView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        gradient.colors = [
+            UIColor.customBlue.cgColor,
+            UIColor.customRed.cgColor
+        ]
 
+        gradient.locations = [0.1, 0.8]
+        gradient.frame = squareView.bounds
+        gradient.cornerRadius = 16
+
+        gradient.startPoint = CGPoint(x: 0, y: 0.0)
+        gradient.endPoint = CGPoint(x: 1, y: 1)
+
+        squareView.layer.insertSublayer(gradient, at: 0)
+
+        squareView.layer.shadowColor = UIColor.black.cgColor
+        squareView.layer.shadowOpacity = 0.25
+        squareView.layer.shadowOffset = CGSize(width: 0.0, height: 10.0)
+        squareView.layer.shadowRadius = 10
+    }
 }
-

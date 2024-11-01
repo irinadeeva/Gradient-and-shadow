b0eeb219f74f41e1eb931ad0e1c718192618a55c
irinadeeva <deeva97@me.com>
2024-11-01 20:10:24 +0300

    added  functions to improve readability

--- Task1 Shadow/ViewController.swift
+++ Task1 Shadow/ViewController.swift
@@ -14,6 +14,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        setUpSquareView()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        setUpGradientShadow()
+    }
+
+    private func setUpSquareView() {
         squareView.layer.cornerRadius = 16
 
         squareView.translatesAutoresizingMaskIntoConstraints = false
@@ -25,17 +35,16 @@ class ViewController: UIViewController {
         squareView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 100).isActive = true
     }
 
-    override func viewDidAppear(_ animated: Bool) {
+    private func setUpGradientShadow() {
         gradient.colors = [
             UIColor.customBlue.cgColor,
             UIColor.customRed.cgColor
         ]
 
-        gradient.locations = [0.1, 0.8]
         gradient.frame = squareView.bounds
         gradient.cornerRadius = 16
-
-        gradient.startPoint = CGPoint(x: 0, y: 0.0)
+        gradient.locations = [0.2, 0.8]
+        gradient.startPoint = CGPoint(x: 0, y: 0)
         gradient.endPoint = CGPoint(x: 1, y: 1)
 
         squareView.layer.insertSublayer(gradient, at: 0)
@@ -44,5 +53,9 @@ class ViewController: UIViewController {
         squareView.layer.shadowOpacity = 0.25
         squareView.layer.shadowOffset = CGSize(width: 0.0, height: 10.0)
         squareView.layer.shadowRadius = 10
+        squareView.layer.shadowPath = UIBezierPath(
+            roundedRect: squareView.bounds,
+            cornerRadius: squareView.layer.cornerRadius
+        ).cgPath
     }
 }
